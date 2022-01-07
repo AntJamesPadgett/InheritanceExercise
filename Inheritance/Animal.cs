01e6340c5fd80c339210bab7372bfda14445ac45
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-07 11:30:34 -0600

    Completed exercise

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace Inheritance
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public bool IsAlive { get; set; }
+        public int Age { get; set; }
+        public int LegCount { get; set; }
+        public string LandSeaAir { get; set; }
+    }
+
+
+
+
+
+
+
+}
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritance
+{
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+
+        }
+        public bool IsColdBlooded { set; get; }
+        public bool IsScaly { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+
+
+
+
+    }
+}
